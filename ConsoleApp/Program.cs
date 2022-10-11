02b9307361a57f07569154060e911bb8d2e6d904
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-10-11 17:06:56 +0200

    skeleton

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using ClassLibrary1;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "sample.ExamCard";
+            if (args.Length > 0)
+                input = args[0];
+
+            Console.WriteLine("Hello World!");
+
+            {
+                var myObject = new Class1();
+                // Serializes a class named Group as a SOAP message.  
+                XmlTypeMapping myTypeMapping =
+                    new SoapReflectionImporter().ImportTypeMapping(typeof(Class1));
+                XmlSerializer mySerializer = new XmlSerializer(myTypeMapping);
+                // To write to a file, create a StreamWriter object.  
+                StreamWriter myWriter = new StreamWriter("myFileName.xml");
+                mySerializer.Serialize(myWriter, myObject);
+                myWriter.Close();
+            }
+
+            {
+                // Construct an instance of the XmlSerializer with the type
+                // of object that is being deserialized.
+                var mySerializer = new XmlSerializer(typeof(Class1));
+                // To read the file, create a FileStream.
+                using var myFileStream = new FileStream(input, FileMode.Open);
+                // Call the Deserialize method and cast to the object type.
+                var myObject = (Class1)mySerializer.Deserialize(myFileStream);
+            }
+        }
+    }
+}
