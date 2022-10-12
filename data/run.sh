5d887f9556e7733632fda4d0f4b9dfcb1c30211e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-10-12 09:24:44 +0200

    minor testing

--- data/run.sh
+++ data/run.sh
@@ -3,3 +3,10 @@
 set -x
 
 trang -I xml -O xsd *.ExamCard ../xsd/examcard.xsd
+
+# dogfooding
+trang -I xml -O rng *.ExamCard examcard.rng
+
+for xml in *.ExamCard; do
+	jing examcard.rng $xml
+done
