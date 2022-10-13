82f336e0f1e42d42566063d02e8e631ca46164f3
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-10-13 14:03:33 +0200

    WIP

--- ClassLibrary1/ExamCard.cs
+++ ClassLibrary1/ExamCard.cs
@@ -2,12 +2,16 @@
 using Philips.PmsMR.Platform.ECPlatform;
 using Philips.PmsMR.Platform.ScannerContext;
 using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.Serialization;
 
 namespace Philips.PmsMR.ExamCards.ECModel
 {
-    [Serializable]
-    [System.Xml.Serialization.SoapTypeAttribute(Namespace = "urn:CHAMADO")]
-    public class ExamCard
+    [System.Serializable]
+    [System.Runtime.Remoting.Metadata.SoapType(XmlNamespace = "http://schemas.microsoft.com/clr/nsassem/Philips.PmsMR.ExamCards.ECModel/philips.pmsmr.examcards.ecmodel_cs")]
+    //[System.Xml.Serialization.XmlRoot(Namespace = "foobar", IsNullable = false)]
+    public class ExamCard  /*: ISerializable */
     {
         public string name;
         public ECInstrumentList instrumentList;
@@ -30,6 +34,72 @@ namespace Philips.PmsMR.ExamCards.ECModel
         public object paediatric;
         public object patientWeight;
         public object selectedCoils;*/
+
+        /*
+        // versioning:
+        private int memberCount;
+
+        public ExamCard()
+        {
+        }
+
+        protected ExamCard(SerializationInfo info, StreamingContext context)
+        {
+            memberCount = info.MemberCount;
+            Debug.Assert(memberCount == 15);
+
+            foreach (SerializationEntry entry in info)
+            {
+                string name = entry.Name;
+                object value = entry.Value;
+                Type type = this.GetType();
+                PropertyInfo[] dbg1 = type.GetProperties();
+                MemberInfo[] dbg2 = type.GetMembers();
+                PropertyInfo propInfo = this.GetType().GetProperty(name);
+                MemberInfo[] propInfo2 = this.GetType().GetMember(name);
+                Debug.Assert(propInfo2.Length == 1);
+                MemberInfo memberInfo = propInfo2[0];
+                Debug.Assert(memberInfo.MemberType == MemberTypes.Field);
+                FieldInfo fieldInfo = (FieldInfo)memberInfo;
+                Type fieldType = fieldInfo.FieldType;
+                switch (fieldInfo.FieldType.Name)
+                {
+                    case "Int32":
+                        ((FieldInfo)memberInfo).SetValue(this, Int32.Parse((string)value));
+                        break;
+                    case "Boolean":
+                        ((FieldInfo)memberInfo).SetValue(this, Boolean.Parse((string)value));
+                        break;
+                    default:
+                        Debug.Assert(fieldInfo.FieldType.Equals(entry.ObjectType));
+                        ((FieldInfo)memberInfo).SetValue(this, value);
+                        break;
+                }
+            }
+
+        }
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            Type type = this.GetType();
+            {
+                MemberInfo[] members = type.GetMembers();
+                foreach (var member in members)
+                {
+                    if (member.MemberType == MemberTypes.Field)
+                    {
+                        FieldInfo field = ((FieldInfo)member);
+                        string name = member.Name;
+                        //if (fieldNames.Contains(name))
+                        {
+                            Debug.Assert(!name.Equals("memberCount"));
+                            object value = field.GetValue(this);
+                            info.AddValue(name, value);
+                        }
+                    }
+                }
+            }
+
+        }*/
     }
     [Serializable]
     public class ECInstrumentList
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,11 +15,20 @@ using System.Xml.Serialization;
 
 namespace ConsoleApp
 {
+    [Serializable]
+    [System.Runtime.Remoting.Metadata.SoapType(XmlNamespace = "MY_NAMESPACE")]
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public string _Address;
+        public string _ZIPCode;
+    }
     class Program
     {
         public static void Main()
         {
-            {
+            /*{
                 ExamCard myGroup = new ExamCard();
                 myGroup.name = "SR_ADULT_007";
                 myGroup.patientOrientation = new PatientPosition();
@@ -30,14 +39,35 @@ namespace ConsoleApp
                 RelationshipManagerData r = new RelationshipManagerData();
                 SingleScan s = new SingleScan();
                 ScanProcedure ss = new ScanProcedure();
-                DynamicScanDelayMode d = new DynamicScanDelayMode();
+                //DynamicScanDelayMode d = new DynamicScanDelayMode();
 
                 FileStream stmCar = new FileStream("wip.ExamCard", FileMode.Create);
                 SoapFormatter sopCar = new SoapFormatter();
 
                 sopCar.Serialize(stmCar, myGroup);
+            }*/
+            /*{
+                //Person p = new Person();
+                ExamCard p = new ExamCard();
+                string path = "myfile.soap";
+                using (System.IO.FileStream fs = new System.IO.FileStream(path, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write))
+                {
+                    System.Runtime.Serialization.Formatters.Soap.SoapFormatter
+                    f = new System.Runtime.Serialization.Formatters.Soap.SoapFormatter();
+                    f.Serialize(fs, p);
+                    fs.Close();
+                }
             }
+            {
+                ExamCard examCard = new ExamCard();
+                examCard.name = "SR_ADULT_007";
+                examCard.patientOrientation = new PatientPosition();
+                examCard.mainSystemType = new MainSysType();
 
+                FileStream fileStream = new FileStream("junk.ExamCard", FileMode.Create);
+                SoapFormatter soapFormatter = new SoapFormatter();
+                soapFormatter.Serialize(fileStream, examCard);
+            }*/
             {
                 //string filename = "data/PHANTOM_QT1_SLU_20151230.ExamCard";
                 string filename = "data/SR_ADULT_007.ExamCard";
@@ -46,11 +76,11 @@ namespace ConsoleApp
                 //string filename = "data/spine_generic_philips_R53.ExamCard";
                 FileStream stmCar = new FileStream(filename, FileMode.Open);
                 SoapFormatter sopCar = new SoapFormatter();
-                ExamCard vehicle = (ExamCard)sopCar.Deserialize(stmCar);
+                ExamCard examCard = (ExamCard)sopCar.Deserialize(stmCar);
 
-                FileStream stmCar2 = new FileStream("roundtrip.ExamCard", FileMode.Create);
-                SoapFormatter sopCar2 = new SoapFormatter();
-                sopCar2.Serialize(stmCar2, vehicle);
+                FileStream fileStream = new FileStream("roundtrip.ExamCard", FileMode.Create);
+                SoapFormatter soapFormatter = new SoapFormatter();
+                soapFormatter.Serialize(fileStream, examCard);
             }
         }
     }
