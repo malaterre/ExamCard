02a8ebb5afa2c249ff62b042f358c4a1718bc544
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-10-17 15:18:10 +0200

    minor fix

--- data/SR_ADULT_007/dump.c
+++ data/SR_ADULT_007/dump.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -12,9 +13,40 @@ struct header {
   uint32_t numparams;
 };
 
-void print_header(struct header *h) {
+#define PACK __attribute__((__packed__))
+struct PACK param {
+  char name[32 + 1];
+  uint8_t for_disp;
+  uint32_t type;
+  uint32_t dim;
+  uint32_t v4;
+  uint32_t offset;
+};
+
+static void print_header(struct header *h) {
   printf("%u %u %u %u %u %u %u %u\n", h->v1, h->nints, h->v4, h->nfloats, h->v6,
          h->nstrings, h->v8, h->numparams);
+}
+
+enum ParamType {
+  Float = 0x0,
+  Integer = 0x1,
+  String = 0x2,
+  Enum = 0x4,
+};
+
+static const char *ParamTypes[] = {
+    "float", "integer", "string",
+    NULL, // 3
+    "enum",
+};
+
+static void print_param(struct param *p) {
+  assert(p->for_disp == 0x0 || p->for_disp == 0x1);
+  assert(p->type == 0x0 || p->type == 0x1 || p->type == 0x2 || p->type == 0x4);
+  const char *type_str = ParamTypes[p->type];
+  printf("%-32s\tdisp:%u\ttype:%s\tsize:%u\toffset1:0x%04x\toffset2:0x%04x\n",
+         p->name, p->for_disp, type_str, p->dim, p->v4, p->offset);
 }
 
 int main(int argc, char *argv[]) {
@@ -24,8 +56,15 @@ int main(int argc, char *argv[]) {
 
   FILE *stream = fopen(filename, "rb");
   struct header header;
+  assert(sizeof header == 32);
   size_t ret = fread(&header, sizeof header, 1, stream);
   print_header(&header);
+  for (int p = 0; p < header.numparams; ++p) {
+    struct param param;
+    assert(sizeof param == 50);
+    ret = fread(&param, sizeof param, 1, stream);
+    print_param(&param);
+  }
   fclose(stream);
   return 0;
 }
