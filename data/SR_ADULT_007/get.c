c881bd551d07b5626f3ed03d101fab37bc412a1b
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-11-10 10:27:16 +0100

    cleanup

--- data/SR_ADULT_007/get.c
+++ data/SR_ADULT_007/get.c
@@ -4,35 +4,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 static const char all[] =
     "NOT_USED1,SHORT,SHORTEST,USER_DEF,NOT_USED3,INPHASE,OUTPHASE";
+
 bool get_enum(char *buffer, size_t buf_len, const char *input, int j) {
-  const char *start = input;
-  size_t len = strlen(input);
-  const char *output = start;
+  if (j < 0) {
+    *buffer = 0;
+    return false;
+  }
+  const size_t len = strlen(input);
+  const char *output = input;
   for (int i = 0; i < j; ++i) {
-    assert(start != NULL);
-    const char *tmp = strchr(start, ',');
+    assert(output != NULL);
+    const char *tmp = strchr(output, ',');
     if (tmp == NULL) {
       *buffer = 0;
       return false;
     }
     output = tmp + 1;
-    start = output;
   }
-  char *beg = buffer;
-  const char *cur = output;
-  while (cur < start + len && *cur != ',') {
-    *buffer++ = *cur++;
-    assert((size_t)(buffer - beg) < buf_len);
+  const char *tmp = strchr(output, ',');
+  const size_t to_copy =
+      tmp == NULL ? len - (output - input) : (size_t)(tmp - output);
+  // it is an error is buffer too small to contains trailing null byte:
+  if (to_copy < buf_len) {
+    strncpy(buffer, output, to_copy);
+    buffer[to_copy] = 0;
+  } else {
+    *buffer = 0;
+    return false;
   }
-  *buffer++ = 0;
   return true;
 }
 
 int main(int argc, char *argv[]) {
   int i = atoi(argv[1]);
-  char buffer[512];
+  char buffer[8 * 2];
   printf("%s\n", all);
   if (get_enum(buffer, sizeof buffer, all, i)) {
     printf("%s\n", buffer);
